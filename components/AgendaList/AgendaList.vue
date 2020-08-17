3c4e4f815eb28171fcf67d4b67ccaedb0eb7dd94
André <andre@dev-next.com>
2020-08-16 23:53:35 -0300

    🚧 Create basic components

---
+++ components/AgendaList/AgendaList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="list">//TODO</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.list {
+  padding: 3rem 5rem 1rem 1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        agenda-frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <main-aside></main-aside>
+    <agenda-list></agenda-list>
   </div>
 </template>
 
@@ -31,35 +9,20 @@
 export default {}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
-  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 0.3fr 0.7fr;
+  grid-template-rows: 1fr;
+  gap: 1px 1px;
+  grid-template-areas: 'aside list';
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  div:first-child {
+    grid-area: aside;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  div:last-child {
+    grid-area: list;
+  }
 }
 </style>
